8f6817820596e0a0e00bcecc85119adafc674e03
ovem <ovem@n-ix.com>
2023-01-04 12:36:39 +0200

    added config file

---
+++ bin/config.js
@@ -0,0 +1,13 @@
+const CLIENT_ID = 'CHANGE_IT'
+const CLIENT_SECRET = 'CHANGE_IT'
+const TENANT_ID = 'CHANGE_IT'
+const AAD_ENDPOINT = 'https://login.microsoftonline.com/'
+const GRAPH_ENDPOINT = 'https://graph.microsoft.com/'
+
+module.exports = {
+    CLIENT_ID,
+    CLIENT_SECRET,
+    AAD_ENDPOINT,
+    TENANT_ID,
+    GRAPH_ENDPOINT
+}
